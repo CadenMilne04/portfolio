df7b1abbcb3d5b56b9482fef25f5a8872cdcb96e
Caden Milne <cadenmilne@Cadens-MacBook-Pro.local>
2024-06-02 14:37:11 -0400

    Fixed project card for workout webapp

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ function Projects() {
                 name = "Workout App" 
                 desc="Fully functioning web application with active users to track workout data."
                 img="/WorkoutAppImage.png"
-                logos={["https://user-images.githubusercontent.com/25181517/183897015-94a0https://workout-app-ugjg.onrender.com/58a6-b86e-4e42-a37f-bf92061753e5.png","https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png","https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png", "https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png",]}
+                logos={["https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png","https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png","https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png", "https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png",]}
                 link="https://workout-app-ugjg.onrender.com/"
                 host='/GitHubLogo.png'
             />
