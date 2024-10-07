2d533983d3f219707d3c4e6268d3e6a2df2d6d37
Caden Milne <cadenmilne@Cadens-MacBook-Pro.local>
2024-10-07 11:37:42 -0400

    feat: Added hackathon win and triviatok

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,27 @@ function About() {
     <div className='flex flex-col w-full'>
         <h1 className='m-auto my-6 font-bold text-3xl'>👇 About Me 👇</h1>
         <div className='grid grid-cols-1 sm:grid-cols-2 mb-6'>
-            <img className="w-96 m-auto" src="/headshot.png" alt="" />
+            <img className="w-96 rounded-full m-auto" src="/headshot.png" alt="" />
             <div className='p-5 flex flex-col justify-center'>
                 <h1 className='m-2 text-2xl font-semibold'>👋 Hello! My name is Caden Milne!</h1>
                 <h1 className='m-2 text-2xl font-semibold'>💼 Intern at Carnegie Mellon's Software Engineering Institute</h1>
                 <h1 className='m-2 text-2xl font-semibold'>👨‍🎓 Studying Computer Science at the <span className='text-blue-600'>University of Pittsburgh</span>.</h1>
                 <h1 className='m-2 text-2xl font-semibold'>🙋‍♂️<span className='text-blue-600'>Highly motivated</span>, I strive to <span className='text-blue-600'>make an impact</span> on any team that I work with.</h1>
+            </div>
+        </div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-3 justify-center'>
+            <div className="w-1/2 h-4/5 m-auto">
+              <img src="/SHFirstPlace.jpg" alt="First Place" className="w-full h-full object-cover rounded-lg"/>
+            </div>
+            <div className="w-1/2 h-4/5 m-auto">
+              <img src="/SHSpeaking.jpeg" alt="Speaking" className="w-full h-full object-cover rounded-lg"/>
+            </div>
+            <div className='p-5 flex flex-col justify-center'>
+                <h1 className='m-2 text-2xl font-semibold'>💻 SteelHacks XI Hackathon Winner</h1>
+                <h1 className='m-2 text-2xl font-semibold'>🏆 1st Place over 350+ participants</h1>
+                <h1 className='m-2 text-2xl font-semibold'>🧑‍💻 Google affiliated</h1>
+                <h1 className='m-2 text-2xl font-semibold'>👨Solo Developer</h1>
             </div>
         </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,14 @@ function Projects() {
         <h1 className='m-auto mb-12 font-bold text-3xl'>👇 Projects 👇</h1>
         <div className="flex flex-wrap mb-12 justify-center">
             <Card 
+                name = "TriviaTok" 
+                desc="Fully functioning tiktok style doom-scrollable trivia. AI question generation with Google's Gemini API"
+                img="/TriviaTokImage.png"
+                logos={["https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png","https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png","https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png", "https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png",]}
+                link="https://triviatok.us/"
+                host='/TriviaTokLogo.png'
+            />
+            <Card 
                 name = "NES Emulator" 
                 desc="Fully functioning Nintendo Entertainment System emulator. Which emulates the 6502 processor, ppu, and I/O."
                 img="/NesEmuImage.png"
